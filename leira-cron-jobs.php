48f375e93faddcc89f5c2b5f60ffe1b9fc7b6df3
Ariel Hernandez <arielhr1987@gmail.com>
2020-09-14 22:54:00 -0700

    adding deployer

--- leira-cron-jobs.php
+++ leira-cron-jobs.php
@@ -16,7 +16,7 @@
  * Plugin Name:       Cron Jobs
  * Plugin URI:        https://github.com/arielhr1987/leira-cron-jobs
  * Description:       Cron Jobs is a simple, but powerful plugin that will help you to manage your site cron jobs easily.
- * Version:           1.2.5
+ * Version:           1.2.6
  * Author:            Ariel
  * Author URI:        https://leira.dev
  * License:           GPL-2.0+
@@ -35,7 +35,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'LEIRA_CRON_JOBS_VERSION', '1.2.5' );
+define( 'LEIRA_CRON_JOBS_VERSION', '1.2.6' );
 
 /**
  * The code that runs during plugin activation.
